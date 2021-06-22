ae11cc36b97ddc3542711cb646e89394c71f40d6
codindb <codindb.dev@gmail.com>
2021-06-22 09:50:57 +0200

    Resolved escalier

---
+++ escalier.js
@@ -0,0 +1,15 @@
+const args = process.argv.slice(2);
+/* array of arguments when the script is executed with shell (using node). 
+first 2 arguments sliced because arg1 is 'node' and arg2 is the filename. 
+see https://nodejs.org/en/knowledge/command-line/how-to-parse-command-line-arguments/ for more details. */
+ 
+const numberOfSteps = parseInt(args[0],10);
+
+if (numberOfSteps < 1) {
+   console.log("Enter an argument greater or equal to 1");
+} else {
+   for(let i = numberOfSteps; i > 0; i--) {
+      console.log(" ".repeat(i-1) + "#".repeat(numberOfSteps-(i-1)));
+   }
+}
+
