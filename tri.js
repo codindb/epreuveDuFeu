5e04e5730353091cdd2d5c3125358a77808d2687
codindb <codindb.dev@gmail.com>
2021-06-22 17:37:43 +0200

    args parsed to get array of int instead of strings

--- tri.js
+++ tri.js
@@ -1,13 +1,15 @@
-const args = process.argv.slice(2);
+let args = process.argv.slice(2);
 /* array of arguments when the script is executed with shell (using node). 
 first 2 arguments sliced because arg1 is 'node' and arg2 is the filename. 
 see https://nodejs.org/en/knowledge/command-line/how-to-parse-command-line-arguments/ for more details. */
+
+args = args.map(x => parseInt(x, 10)); // parse the array to integer because the shell arguments are an array of strings
 
 let sortedNumbers = 0;
 
 while (sortedNumbers < args.length) {
    for (let i = 0; i < args.length-sortedNumbers; i++) {
-      if (parseInt(args[i],10) < parseInt(args [i + 1],10)) {
+      if (args[i] < args [i + 1]) {
          let temp;
          temp = args[i];
          args[i] = args[i + 1];
